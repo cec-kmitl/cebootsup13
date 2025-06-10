02225f5ef50e672d5c36f24f5b52cc2c12a1f102
txasw <txasw@outlook.co.th>
2025-06-11 01:43:40 +0700

    066,071,162

---
+++ grader/ex066/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    float a,b,c;
+    scanf("%f\n%f\n%f",&a,&b,&c);
+    printf("%.2f",(a-b)/c);
+    return 0;
+}
---
+++ grader/ex071/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("'\"+--------------------------------+'\"\n|File:\\\"C:\\\\user\\Card\"so_handsome\"\\\\vault\\\\file.txt\\\"|\n|\"TOP SECRET FILE\"|\n|Alert:\\a(Do not trigger it!)/a|\n|Quote:\"\\\\bBackspaceb//\"|\n|Hidden:n/'[Encrypted]'\\n'|\n'\"+--------------------------------+'\"\n");
+    return 0;
+}
